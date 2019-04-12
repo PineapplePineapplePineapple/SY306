a4b043e9f7d3e25ec484be34d4656eef4c4c5ffd
m202496 <m202496@usna.edu>
2019-04-11 21:37:32 -0400

    u

--- message.py
+++ message.py
@@ -8,7 +8,7 @@ class message:
   def addMessage(cursor, content, time):
 
     #create query statement
-    query = "Insert into MESSAGES(content, time) values ('" + content + "','" + time + "')"
+    query = "Insert into MESSAGES(Content, Time) values ('" + content + "','" + time + "')"
     #execute the query
     try:
       cursor.execute(query)
